0fda1a7b2f3563f5e2c3d8bc13c7b23331c15af3
mbelouar <simobelouarraq5@gmail.com>
2025-06-04 17:44:06 +0100

    fix the provisionning of the kubectl and k3s for the server node

---
+++ p1/scripts/kubectl.sh
@@ -0,0 +1,4 @@
+sudo apt-get update && sudo apt-get upgrade -y
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
