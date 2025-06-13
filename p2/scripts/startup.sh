51195781f91029fe230540250fe44ef4b7fccc9d
mbelouar <simobelouarraq5@gmail.com>
2025-06-13 16:53:55 +0100

    i changed the way to expose the service from the command line not yaml file

--- p2/scripts/startup.sh
+++ p2/scripts/startup.sh
@@ -2,7 +2,30 @@
 
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" sh -
 
+sleep 10
+
 kubectl apply -f /vagrant-data/app1.yaml
 kubectl apply -f /vagrant-data/app2.yaml
 kubectl apply -f /vagrant-data/app3.yaml
+
+kubectl expose deployment app1-deployment \
+  --type=ClusterIP \
+  --name=app1-svc \
+  --port=80 \
+  --target-port=80
+
+kubectl expose deployment app2-deployment \
+  --type=ClusterIP \
+  --name=app2-svc \
+  --port=80 \
+  --target-port=80
+
+kubectl expose deployment app3-deployment \
+  --type=ClusterIP \
+  --name=app3-svc \
+  --port=80 \
+  --target-port=80
+
+sleep 5
+
 kubectl apply -f /vagrant-data/ingress.yaml
