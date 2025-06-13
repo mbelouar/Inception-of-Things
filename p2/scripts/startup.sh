4c2e8bbe10915f613b249d4e58b7850f1c169e58
mbelouar <simobelouarraq5@gmail.com>
2025-06-13 16:44:42 +0100

    add the kubectl apply command in the script to automatically create Pods, deployments, Services, ReplicaSets and Ingress

--- p2/scripts/startup.sh
+++ p2/scripts/startup.sh
@@ -2,3 +2,7 @@
 
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" sh -
 
+kubectl apply -f /vagrant-data/app1.yaml
+kubectl apply -f /vagrant-data/app2.yaml
+kubectl apply -f /vagrant-data/app3.yaml
+kubectl apply -f /vagrant-data/ingress.yaml
